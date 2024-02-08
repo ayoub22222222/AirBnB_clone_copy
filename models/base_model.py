2516e960bb714d78980dd909e0798aaa11dd3234
Ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-02-08 05:36:23 -0800

    Basemodel_struct

--- models/base_model.py
+++ models/base_model.py
@@ -3,5 +3,25 @@
 
 
 class BaseModel:
-    pass
+    """description the class 
+    compenent
+    """
+    def __init__(self):
+        self.id = id
+        self.created_at = #assigned with the current date time
+        self.updated_at = #assign with the current datetime when an instance is created
+
+    def __str__(self):
+        """ should print name of the class
+        the id and dict """
+        pass
+    def save(self):
+        """ update the public instance attribute update_at
+        with the current date time """
+        pass
+    def to_dict(self):
+        """ return a dict ontaining all keys/values of 
+        __dict__ of the instance"""
+        pass
+
 
