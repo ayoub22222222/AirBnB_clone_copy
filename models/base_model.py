6a0a870581c43f8949da352eec0916f80e9ab40c
Ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-02-08 12:18:18 -0800

    new_updates

--- models/base_model.py
+++ models/base_model.py
@@ -11,21 +11,22 @@ class BaseModel:
     def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now().isoformat()
-        self.updated_at = #assign with the current datetime when an instance is created
+        self.updated_at = datetime.now().isoformat()
 
     def __str__(self):
         """ should print name of the class
         the id and dict """
-        print("[<class name>] (<self.id>) <self.__dict__>".format(BaseModel.__name__, self.id, self.__dict__))
-        pass
+        return "[{:s}] ({:s}) {:s}".format(BaseModel.__name__, str(self.id), str(self.__dict__))
+        
 
     def save(self):
         """ update the public instance attribute update_at
         with the current date time """
-        pass
+        self.updated_at = datetime.now().isoformat()
+        
 
     def to_dict(self):
         """ return a dict ontaining all keys/values of 
         __dict__ of the instance"""
-        pass
+        return self.__dict__
 
