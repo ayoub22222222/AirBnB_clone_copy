5fac43ec02de6b247f6a0b3de77d2d5e42909e84
Ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-02-10 15:43:32 -0800

    test

--- models/base_model.py
+++ models/base_model.py
@@ -2,22 +2,27 @@
 """ explain the purpose of the class """
 import uuid
 from datetime import datetime
-import json
+
 
 class BaseModel:
     """description the class
     compenent
     """
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now().isoformat()
-        self.updated_at = datetime.now().isoformat()
+    
+    id = str(uuid.uuid4())
+    created_at = datetime.now().isoformat()
+    updated_at = datetime.now().isoformat()
 
     def __str__(self):
         """ should print name of the class
         the id and dict """
+        copy_dict = self.__class__.__dict__.copy()
+        copy_dict.update(self.__dict__)
+        comp_list = ['__module__', '__doc__', '__str__', 
+                '__dict__', 'to_dict', '__weakref__', 'save']
+        filter_dict = {key : value for key, value in copy_dict.items() if key not in comp_list}
         return "[{}] ({}) {}".format(
-                self.__class__.__name__, self.id, json.dumps(self.__dict__))
+                self.__class__.__name__, self.id, self.__class__.__dict__)
 
     def save(self):
         """ update the public instance attribute update_at
@@ -25,8 +30,15 @@ class BaseModel:
         self.updated_at = datetime.now().isoformat()
 
     def to_dict(self):
-        """ return a dict ontaining all keys/values of
-        __dict__ of the instance"""
-        obt = self.__dict__
-        obt["__class__"] = "BaseModel"
-        return obt
+        """
+        Returns a dictionary containing key/value of __dict__ for an instance
+        """
+        comp_list = ['__module__', '__doc__', '__str__',
+                '__dict__', 'to_dict', '__weakref__', 'save']
+        new_ob = self.__class__.__dict__.copy()
+        new_ob.update(self.__dict__)
+        new_one = {key : value for key, value in new_ob.items() if key not in comp_list}
+        new_one['__class__'] = self.__class__.__name__
+        new_one['updated_at'] = self.updated_at.isoformat()
+        new_one['created_at'] = self.created_at.isoformat()
+        return new_one
