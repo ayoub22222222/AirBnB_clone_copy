b163e2abb7069b03cdc732d5ef32c3e52615684e
Ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-02-09 17:02:33 -0800

    serilization

---
+++ models/engine/file_storage.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" this file conatins a class
+that """
+
+
+
+class FileStorage:
+    """this class is for creating"""
+    __file_path = ''
+    __file_path = {}
+
+    def all(self):
+        """ description for all() methode"""
+        pass
+
+    def new(self, obj):
+        """description for new() methode """
+        pass
+
+    def save(self):
+        """ description for save() methode"""
+        pass
+
+    def reload(self):
+        """description for reload methode"""
+        pass
+
