e051feee4539c394507b468142eddcba5509eeb6
Ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-02-10 19:34:33 -0800

    3yat

--- models/base_model.py
+++ models/base_model.py
@@ -10,8 +10,8 @@ class BaseModel:
     """
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
 
     def __str__(self):
         """ should print name of the class
@@ -22,7 +22,7 @@ class BaseModel:
     def save(self):
         """ update the public instance attribute update_at
         with the current date time """
-        self.updated_at = datetime.utcnow()
+        self.updated_at = datetime.today()
 
     def to_dict(self):
         """
