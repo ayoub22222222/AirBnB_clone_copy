d9788f0932a97086584a71565075ac8f433eb17f
Ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-02-10 16:11:37 -0800

    test_

--- models/base_model.py
+++ models/base_model.py
@@ -8,7 +8,6 @@ class BaseModel:
     """description the class
     compenent
     """
-    
     id = str(uuid.uuid4())
     created_at = datetime.now().isoformat()
     updated_at = datetime.now().isoformat()
@@ -18,9 +17,12 @@ class BaseModel:
         the id and dict """
         copy_dict = self.__class__.__dict__.copy()
         copy_dict.update(self.__dict__)
-        comp_list = ['__module__', '__doc__', '__str__', 
+        comp_list = [
+                '__module__', '__doc__', '__str__',
                 '__dict__', 'to_dict', '__weakref__', 'save']
-        filter_dict = {key : value for key, value in copy_dict.items() if key not in comp_list}
+        filter_dict = {
+                key:value for key,
+                value in copy_dict.items() if key not in comp_list}
         return "[{}] ({}) {}".format(
                 self.__class__.__name__, self.id, self.__class__.__dict__)
 
@@ -33,12 +35,15 @@ class BaseModel:
         """
         Returns a dictionary containing key/value of __dict__ for an instance
         """
-        comp_list = ['__module__', '__doc__', '__str__',
+        comp_list = [
+                '__module__', '__doc__', '__str__',
                 '__dict__', 'to_dict', '__weakref__', 'save']
         new_ob = self.__class__.__dict__.copy()
         new_ob.update(self.__dict__)
-        new_one = {key : value for key, value in new_ob.items() if key not in comp_list}
+        new_one = {
+                key:value for key,
+                value in new_ob.items() if key not in comp_list}
         new_one['__class__'] = self.__class__.__name__
-        new_one['updated_at'] = self.updated_at.isoformat()
-        new_one['created_at'] = self.created_at.isoformat()
+        new_one['updated_at'] = self.updated_at
+        new_one['created_at'] = self.created_at
         return new_one
