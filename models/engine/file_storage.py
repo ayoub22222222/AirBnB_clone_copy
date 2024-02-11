8944dea5adac0420c39a6b68af7c6562154f5b56
Ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-02-11 12:06:37 -0800

    file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ this file conatins a class
 that """
-
+import json
+import models
 
 
 class FileStorage:
@@ -11,17 +12,33 @@ class FileStorage:
 
     def all(self):
         """ description for all() methode"""
-        pass
+        return self.__objects
 
     def new(self, obj):
         """description for new() methode """
-        pass
+        i = str(obj.__class__.__name__) + "." + str(obj)
+        val_dct = obj
+        FileStorage.__objects[i] = val_dct
 
     def save(self):
-        """ description for save() methode"""
-        pass
+        """ description for save() methode serelize"""
+        fl_pth = FileStorage.__file_path
+        dt_dct = FileStorage.__objects
+        store_dict = {
+                key: value for key,
+                value in dt_dct.items() key = value.to_dict()}
+
+        with open(fl_pth, mode='w', encoding="UTF8") as f:
+            json.dump(store_dict, f)
 
     def reload(self):
-        """description for reload methode"""
-        pass
-
+        """description for reload methode decerilize"""
+        try:
+            with open(FileStorage.__file_path, encoding="UTF8") as f:
+                FileStorage.__objects = json.load(f)
+            for key, value in FileStorage.__objects.items():
+                cls_name = value["__class__"]
+                cls_name = models.classes[class_name]
+                FileStorage.__objects[key] = cls_name(**value)
+            except FileNotFoundError:
+                pass
