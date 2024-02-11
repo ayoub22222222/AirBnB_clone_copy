9f4ee0405d67737e4a0ed0eb4d235a4f7c0c3aa3
Ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-02-10 19:07:27 -0800

    i_think_the_right_one

--- models/base_model.py
+++ models/base_model.py
@@ -8,23 +8,16 @@ class BaseModel:
     """description the class
     compenent
     """
-    id = str(uuid.uuid4())
-    created_at = datetime.now()
-    updated_at = datetime.now()
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """ should print name of the class
         the id and dict """
-        copy_dict = self.__dict__.copy()
-        copy_dict.update(self.__class__.__dict__)
-        comp_list = [
-                '__module__', '__doc__', '__str__',
-                '__dict__', 'to_dict', '__weakref__', 'save']
-        filter_dict = {
-                key: value for key,
-                value in copy_dict.items() if key not in comp_list}
         return "[{}] ({}) {}".format(
-                self.__class__.__name__, self.id, filter_dict)
+                self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """ update the public instance attribute update_at
@@ -35,14 +28,7 @@ class BaseModel:
         """
         Returns a dictionary containing key/value of __dict__ for an instance
         """
-        comp_list1 = [
-                '__module__', '__doc__', '__str__',
-                '__dict__', 'to_dict', '__weakref__', 'save']
-        new_ob = self.__dict__.copy()
-        new_ob.update(self.__class__.__dict__)
-        new_one = {
-                key: value for key,
-                value in new_ob.items() if key not in comp_list1}
+        new_one = self.__dict__.copy()
         new_one['__class__'] = self.__class__.__name__
         new_one['updated_at'] = self.updated_at.isoformat()
         new_one['created_at'] = self.created_at.isoformat()
