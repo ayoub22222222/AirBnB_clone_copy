1e143edae2eaa2a444b4355ebdff37658d88a571
Ayoub22222222 <essbaiayoubmohammed@gmail.com>
2024-02-11 17:51:49 -0800

    task_9

--- console.py
+++ console.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 """ Module for the entry point of the command interpreter """
 import cmd
-
+import json
+import shlex
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 class HBNBCommand(cmd.Cmd):
     """Class for the command interpreter"""
@@ -23,6 +32,30 @@ class HBNBCommand(cmd.Cmd):
         """"Provide help"""
         print("Quit command to exit the program")
 
+    def do_show(self, args):
+        """description for the methode"""
+        args = shlex.split(args)
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        if len(args) == 1:
+            print("** instance id missing **")
+            return
+        storage = FileStorage()
+        storage.reload()
+        o_dict = storage.all()
+        try:
+            eval(args[0])
+        except NameError:
+            print("** class doesn't exist **")
+            return
+        key = args[0] + "." + args[1]
+        key = args[0] + "." + args[1]
+        try:
+            value = o_dict[key]
+            print(value)
+        except KeyError:
+            print("** no instance found **")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
